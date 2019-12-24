641bd2b8c90c98e1f4a63547ebe4d8283ee1e7ec
Anastasiia Vatsyk <avgusta92@gmail.com>
2019-12-24 20:27:06 +0200

    HW6

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -5,26 +5,24 @@ let c = prompt('Please input c');
 if ( !a || !b || !c || isNaN(a) || isNaN(b) || isNaN(c) || a === 0 || b === 0 || c === 0 ) {
     console.log('Invalid input data');
 } else {
-    let numberTwo = 2;
-    let numberFour = 4;
+    const numberTwo = 2;
+    const numberFour = 4;
     let discriminant = b * b - numberFour * a * c;
-    let xEqualsZero = -b / (numberTwo * a);
-    let xLargerZero1 = (-b + Math.sqrt(discriminant)) / (numberTwo * a);
-    let xLargerZero2 = (-b - Math.sqrt(discriminant)) / (numberTwo * a);
 
     if (discriminant < 0) {
         console.log('no solution');
     }
 
     if (discriminant === 0) {
-        console.log('x = ‘' + xEqualsZero + '’');
+        let xEqualsZero = -b / (numberTwo * a);
+
+        console.log('x = ' + xEqualsZero);
     }
 
     if (discriminant > 0) {
-        console.log('x1 = ' + xLargerZero1 + ' and, x2 = ' + xLargerZero2 );
+        let xLargerZero1 = (-b + Math.sqrt(discriminant)) / (numberTwo * a);
+        let xLargerZero2 = (-b - Math.sqrt(discriminant)) / (numberTwo * a);
+
+        console.log('x1 = ' + xLargerZero1 + ' and, x2 = ' + xLargerZero2);
     }
 }
-
-// Округлюйте значення до найближчого цілого числа.
-
-// eslint ./js/task1.js
