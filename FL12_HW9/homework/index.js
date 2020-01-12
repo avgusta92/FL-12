47311b3050ec5bbb0ebd952137f051edc183b820
Anastasiia Vatsyk <avgusta92@gmail.com>
2020-01-13 01:55:14 +0200

    Update index.js

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,6 +1,5 @@
 function convert() {
     let result1 = [];
-
     for (let i = 0; i < arguments.length; i++) {
         if (typeof arguments[i] === 'string') {
             result1.push(parseInt(arguments[i]));
@@ -9,10 +8,8 @@ function convert() {
             result1.push(x);
         }
     }
-
     return result1;
 }
-
 
 function executeforEach(a, b) {
     for (let i = 0; i < a.length; i++) {
@@ -20,10 +17,8 @@ function executeforEach(a, b) {
     }
 }
 
-
 function mapArray(array, func) {
     let result3 = [];
-
     function newFun(el) {
         if (typeof el === 'string') {
             result3.push(func(parseInt(el)));
@@ -31,73 +26,58 @@ function mapArray(array, func) {
             result3.push(func(el));
         }
     }
-
     executeforEach(array, newFun);
     return result3;
 }
 
-
 function filterArray(array, func) {
     let result4 = [];
-
     function newFun(el) {
         if (func(el) === true) {
             result4.push(el);
         }
     }
-
     executeforEach(array, newFun);
     return result4;
 }
 
-
 function flipOver(a) {
     let result5 = '';
-
     for (let i = a.length - 1; i >= 0; i--) {
         result5 += a[i];
     }
     return result5;
 }
 
-
 function makeListFromRange(a) {
     let result6 = [];
-
     for (let i = a[0]; i <= a[1]; i++) {
         result6.push(i);
     }
     return result6;
 }
 
-
 function getArrayOfKeys(array, b) {
     let result7 = [];
-
     function newFun(el) {
         result7.push(el[b]);
     }
-
     executeforEach(array, newFun);
     return result7;
 }
 
-
 function substitute(array) {
     let result8 = [];
     const number30 = 30;
-
     function func(el) {
         if (el > number30) {
             result8.push(el);
             result8.push('*');
         }
     }
-
     mapArray(array, func);
     return result8;
 }
-
 
 function getPastDay(a, b) {
     let newDate = new Date(a);
@@ -105,28 +85,22 @@ function getPastDay(a, b) {
     return newDate;
 }
 
-
 function formatDate(a) {
     const ten = 10;
-    
     let newDate = '';
     let zeroHours = '';
     let zeroMinutes = '';
-
     let date = a.getDate();
     let year = a.getFullYear();
     let month = a.getMonth() + 1;
     let hours = a.getHours();
     let minutes = a.getMinutes();
-
     if (hours < ten) {
         zeroHours = '0';
-    } 
+    }
     if (minutes < ten) {
         zeroMinutes = '0';
     }
-
     newDate = year + '/' + month + '/' + date + ' ' + zeroHours + hours + ':' + zeroMinutes + minutes;
-
     return newDate;
 }
